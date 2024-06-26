2f57db21b7096a31427d244abb326fa4ae3630e9
Henning Janssen <hjanssen@ethz.ch>
2024-06-26 15:43:36 +0200

    pypi version 1.2.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(name='python-libtiepie',
-      version='1.2.1',
+      version='1.2.5',
       description='Python bindings for LibTiePie',
       license='MIT',
       author='TiePie engineering',
